58b07719c61f97c25365e957fa719b8e08238918
rosengau <rosengau@LAPTOP-6D8756L9>
2023-05-30 08:38:04 -0700

    Add project files.

---
+++ MeniscusTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MeniscusTracking;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = args.Length > 1 ? Convert.ToInt32(args[1]) : 0;
+            var end = args.Length > 2 ? Convert.ToInt32(args[2]): 0;
+            var c = new Class1();
+            c.MeniscusTop(args[0], start, end);
+        }
+    }
+}
